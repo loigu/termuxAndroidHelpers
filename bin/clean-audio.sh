c6d88a92e427fa977f24c51be27c9cfe3b4b12c7
loki <jzouhar@netio/eu>
2019-10-08 14:12:35 +0630

    inplace option

--- bin/clean-audio.sh
+++ bin/clean-audio.sh
@@ -9,12 +9,16 @@ if [ -z "$1" -o "$1" = "-h" ]; then
 	echo -e "\tlowpass=$lowpass"
 	echo -e "\tquality=$quality"
 	echo "$0 from [to]"
+	echo "to == -i -> inplace"
 	exit 0
 fi
 
 from="$1"
 to="$2"
-[ -z "${to}" ] && to=$(dirname "$1")/clean-$(basename "$1")
+[ "$to" = "-i" ] && inplace=1 && to=""
+[ -z "${to}" ] && to=$(dirname "$1")/clean-$(basename "${1%.*}").mp3
 
 ffmpeg ${extra} -i "${from}" -codec:a libmp3lame -ac 1 -ar 16000 -q:a ${quality} -map 0 -af "lowpass=f=${lowpass},highpass=f=${highpass}" "${to}"
+
+[ "$?" = 0 -a "${inplace}" = 1 ] && mv "${to}" "${from}"
 
