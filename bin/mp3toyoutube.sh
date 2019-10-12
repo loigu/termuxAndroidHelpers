1a15c812002b48b5fde620634b2c9e043953b8ee
loki <jzouhar@netio/eu>
2019-10-12 18:45:22 +0630

    support both lowercase and uppercase in extension

--- bin/mp3toyoutube.sh
+++ bin/mp3toyoutube.sh
@@ -9,7 +9,7 @@ audio="$1"
 image="$2"
 out="$3"
 
-[ -z "$out" ] && out="${audio%%.mp3}.mov"
+[ -z "$out" ] && out="${audio%%.[Mm][Pp]3}.mov"
 
 ffmpeg -i "$audio" -loop 1 -r 1 -i "$image"  -c:a copy -c:v libx264 -preset fast -tune stillimage -shortest "$out"
 
