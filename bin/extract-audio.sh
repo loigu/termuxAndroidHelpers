eb9dcd0ed8b7b9ea1bb8196c7439721fc417ed53
bhikkhu nanda <zouhar.jiri@gmail.com>
2020-02-15 14:33:14 +0800

    detection of dir and defaults for parameters

--- bin/extract-audio.sh
+++ bin/extract-audio.sh
@@ -2,7 +2,15 @@
 
 from="$1"
 to="$2"
-[ -z "${to}" ] && to=$(dirname "$1")/$(basename "$1").mp3
+if [ -z "${to}" ]; then
+       	to=$(dirname "$1")/$(basename "$1").mp3
+	to="${to%%.*}.mp3"
+fi
+
+if [ -z "$from" ]; then
+	echo ${basename $0} from [to]
+	exit 1
+fi
 
 ffmpeg -i "${from}" -ac 1 -map 0 -f mp3 -vn -ar 22050 -ab 64000 "${to}"
 
