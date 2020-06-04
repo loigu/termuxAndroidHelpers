115b663d83f9fef4d713d0db15d3fbd1a233ab6d
nanda <nanda.bhikkhu@sasanarakkha.org>
2020-06-04 14:32:51 +0800

    shrink-all-audio.sh: defaults

--- bin/shrink-all-audio.sh
+++ bin/shrink-all-audio.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+[ "$1" = '-h' ] && echo -e"$0 <srcdir> <dstdir>\n\tdefaults: srcdir=.; dstdir=../small" && exit 0
 
-mkdir small
-rm debug.txt success failed
+quality="${quality:-8}"
+lowpass="${lowpass:-5500}"
+highpass="${highpass:-300}"
 
 src="$1"
 out="$2"
 [ -z "$src" ] && src='.'
 [ -z "$out" ] && out='../small'
 
+mkdir -p "${out}"
+rm -rf debug.txt success failed
+
 find "$src" -type d | \
 while read album; do
 	mkdir -p "${out}/${album}"
 	find "${album}" -maxdepth 1 -type f | \
 	while read track; do 
-		if ffmpeg -y -i "${track}" -codec:a libmp3lame -ac 1 -ar 16000 -q:a ${quality} -map 0 -af "lowpass=f=${lowpass},highpass=f=${highpass}" "${out}/${track}" </dev/null &>>debug.txt; then 
+		if ffmpeg -y -i "${src}/${track}" -codec:a libmp3lame -ac 1 -ar 16000 -q:a ${quality} -map 0 -af "lowpass=f=${lowpass},highpass=f=${highpass}" "${out}/${track}" </dev/null &>>debug.txt; then 
 			echo "$track" >> "success"
 			echo -e "$track:\n\tsuccess"
 		else 
