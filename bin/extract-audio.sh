c80808acea4716091dcf3b019187a35128e58fa5
nanda <nanda.bhikkhu@sasanarakkha.org>
2020-07-23 12:38:15 +0800

    audio extract updates

--- bin/extract-audio.sh
+++ bin/extract-audio.sh
@@ -4,14 +4,16 @@
 
 from="$1"
 to="$2"
-if [ -z "${to}" ]; then
-       	to=$(dirname "$1")/$(basename "$1").mp3
-	to="${to%%.*}.mp3"
-fi
 
 if [ -z "$from" ]; then
 	echo ${basename $0} from [to]
 	exit 1
+fi
+
+if [ -z "${to}" ]; then
+	d=$(dirname "$1")
+	f=$(basename "$1")
+	to="$d/${f%%.*}.mp3"
 fi
 
 ffmpeg -i "${from}" -ac 1 -map 0 -f mp3 -vn -ar 22050 -ab 64000 "${to}"
--- bin/shrink-all-audio.sh
+++ bin/shrink-all-audio.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [ "$1" = '-h' ] && echo -e"$0 <srcdir> <dstdir>\n\tdefaults: srcdir=.; dstdir=../small" && exit 0
+basedir=$(pwd)
 
 quality="${quality:-8}"
 lowpass="${lowpass:-5500}"
@@ -11,21 +12,35 @@ out="$2"
 [ -z "$out" ] && out='../small'
 
 mkdir -p "${out}"
-rm -rf debug.txt success failed
+out=$(cd "$out" && pwd)
 
-find "$src" -type d | \
+rm -rf "${out}/debug.txt" "${out}/success" "${out}/failed" "${out}/candidates"
+
+get_size()
+{
+	ls -s "$1" | cut -d ' ' -f 1
+}
+size_diff()
+{
+	expr $(get_size "$1") - $(get_size "$2")
+}
+
+cd "$src"
+find ./ -type d | \
 while read album; do
 	mkdir -p "${out}/${album}"
 	find "${album}" -maxdepth 1 -type f | \
 	while read track; do 
-		if ffmpeg -y -i "${src}/${track}" -codec:a libmp3lame -ac 1 -ar 16000 -q:a ${quality} -map 0 -af "lowpass=f=${lowpass},highpass=f=${highpass}" "${out}/${track}" </dev/null &>>debug.txt; then 
-			echo "$track" >> "success"
+		if ffmpeg -y -i "${track}" -codec:a libmp3lame -ac 1 -ar 16000 -q:a ${quality} -map 0 -af "lowpass=f=${lowpass},highpass=f=${highpass}" "${out}/${track}" </dev/null &>>debug.txt; then 
+			echo "$track" >> "${out}/success"
 			echo -e "$track:\n\tsuccess"
+			[ $(size_diff "${track}" "${out}/${track}") -gt $(($(get_size "${track}") / 10)) ] && echo "${track}" >> "${out}/candidates"
 		else 
-			echo "$track" >> "failed"
+			echo "$track" >> "${out}/failed"
 			echo -e "$track:\n\tfail"
 			rm "${out}/${track}"
 		fi
 	done
 done
 
+cd "$basedir"
