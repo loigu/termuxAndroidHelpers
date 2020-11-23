1a378de2e4a67885e1198d46eeeb8baea602e654
loki <jzouhar@netio/eu>
2020-11-23 16:51:50 +0800

    added help

--- bin/pmkkh-split.sh
+++ bin/pmkkh-split.sh
@@ -4,12 +4,28 @@ fmt_out() {
 	printf '%02d-%s.mp3' "$1" "$2"
 }
 
+print_help() {
+	echo "$(basename \"$0\") [-p prefix] [-i count start] [-h] <in> split1 split2 ..."
+}
+
+i=0
+while true; do
+	case "$1" in
+		-h) print_help; exit 0;;
+		-p) prefix="$2"; shift;;
+		-i) i="$2"; shift;;
+		*) break;;
+	esac
+	shift
+done </dev/null
 in="$1"
-prefix="$2"
-shift 2
+shift
+
+[ -n "$prefix" ] || prefix="${in%.*}"
+
+[ ! -f "$in" ] && print_help && exit 1
 
 b=0
-i=0
 for e in "$@"; do
 	cut-from-mp3.sh "$in" "$b" "$e" "$(fmt_out $i $prefix)"
 	b=$e
