0f7dd5a1f8d6f3b0c8f9416ef35f6b01884fbb89
nanda <nanda.bhikkhu@sasanarakkha.org>
2021-04-01 11:02:00 +0800

    fix bug when getting relative paths

--- bin/copy-in-order.sh
+++ bin/copy-in-order.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 # copy files in name order - ensure it is in the same order on fat (for stupid mp3 players)
 
-IN="$1"
-OUT="$2"
+IN=$(cd "$1" && pwd)
+OUT=$(cd "$2" && pwd)
 
 [ -z "$IN" -o -z "$OUT" -o "$IN" = '-h' ] && echo $(basename "$0") '<indir> <outdir>' && exit
 
 copy_dir()
 {
-	local old_path="$rel_path"
+	local target="$2/$1"
+	[ -d "$target" ] || mkdir -p "$target"
 
-	cd "$1"
-	[ -n "$rel_path" ] && rel_path="$rel_path/$1" || rel_path="$1"
-	[ -d "$OUT/$rel_path" ] || mkdir -p "$OUT/$rel_path"
-	sync -f "$OUT/$rel_path"
-
-	ls -1 | while read item; do
-		[ -f "$OUT/$rel_path/$item" ] && continue
-
-		[ -d "$item" ] && copy_dir "$item"
-		[ -f "$item" ] && cp "$item" "$OUT/$rel_path"  && sync -f "$OUT/$rel_path"
+	find "$1" -xdev -maxdepth 1 -type f | sort | while read f; do
+		cp "$f" "$target"
+		sync -f "$target"
 	done
-
-	cd ..
-	rel_path="$old_path"
 }
 
 OLD_PWD="$(pwd)"
 rel_path=""
 
 cd "$IN"
-copy_dir .
+find ./ -xdev -type d | sort | while read d; do
+	copy_dir "$d" "$OUT"
+done
 cd "$OLD_PWD"
-
