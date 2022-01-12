649d8ee090f4c39fa368b48eb0fa0ab7056adfa2
loki <jzouhar@netio/eu>
2022-01-13 05:20:35 +0530

    better logging & force override by default

--- shortcuts/shd.sh
+++ shortcuts/shd.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+export extra="-v 16 -y"
+
 cd ~/Download/shrink/ || exit 1
 mkdir -p 'done' 'shrunk'
+
 ls -1 *.mp3 *.m4a 2>/dev/null | while read f; do
-	clean-audio.sh "$f" "shrunk/" </dev/null 2>>debug.txt && \
+	echo "$f:"
+	clean-audio.sh "$f" "shrunk/" </dev/null  && \
 		mv "$f" "done/"
 done
