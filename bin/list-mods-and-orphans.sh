efc54b7ba4c2f1ceded1336a698231fbbabe9b90
loki <jzouhar@netio/eu>
2022-01-13 06:25:39 +0530

    silence match, some.todos

--- bin/list-mods-and-orphans.sh
+++ bin/list-mods-and-orphans.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # compare two directories, list modified & orphaned files 
 # TODO: list files found in second dir and not in first too
+# consider sqlite3, size compare for update speedup
 
 [ -z "$1" -o "$1" = "-h" ] && echo "[extra='-L' constraints='-not -path whatever']" $(basename "$0") [-f -u -m] "<indir1|infile1.md5> label1 <indir2|infile2.md5> label2 out-prefix" && exit
 
 #todo check relative path comp
 #todo: proper readarg
 
-while expr match "$1" '^-[fum]$'; do
+while expr match "$1" '^-[fum]$' &>/dev/null; do
 	[ "$1" = '-f' ] && FORCE=1 && shift
 	[ "$1" = '-u' ] && UPDATE=1 && shift
 	[ "$1" = '-m' ] && MD_ONLY=1 && shift
@@ -21,8 +22,8 @@ OUT="$5"
 
 [ -n "$OUT" ] && OUT="$OUT-"
 
-expr match "${IN1}" '.*/' && IN1="${IN1%/}"
-expr match "${IN2}" '.*/' && IN2="${IN2%/}"
+expr match "${IN1}" '.*/' &>/dev/null && IN1="${IN1%/}"
+expr match "${IN2}" '.*/' &>/dev/null && IN2="${IN2%/}"
 
 make_list()
 {
@@ -61,7 +62,11 @@ process_lists()
 	done 
 }
 
-#todo: add new, move, replace changed (ask first option), delete removed (ask first)
+#todo: +dry run / create batch
+#add new (add duplicates)
+#move (beware of duplicates)
+#replace changed (ask first option)
+#delete removed (ask first, cleanup empty dirs)
 
 if [ -d "$IN1" ]; then
 	[ "$FORCE" = 1 ] && rm -f "$IN1.md5"
