d252ef287cc88b39fd5c09c06d9109659c52a2fa
loki <jzouhar@netio/eu>
2022-09-07 08:41:17 +0530

    md uodate formatting

--- bin/md_git_update.sh
+++ bin/md_git_update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
 cp -r ~/Documents/kahambana/macroDroid/conf/* ~/termuxAndroidHelpers/macrodroid/kuta-well/
 tmp=$(mktemp)
-find ~/termuxAndroidHelpers/macrodroid/kuta-well/ -type f |while read f; do json_prettyprint "$f" "${tmp}"||echo $f failed; mv "${tmp}" "$f"; done
+
+find ~/termuxAndroidHelpers/macrodroid/kuta-well/ -type f | \
+	while read f; do 
+		json_prettyprint "$f" "${tmp}" ||echo $f faile
+		mv "${tmp}" "$f"
+	done
