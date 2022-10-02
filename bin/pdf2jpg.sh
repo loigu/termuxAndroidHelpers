2bbe1bc8134a1c9bd462f8f440636b0ed54dc17e
nanda <nanda.bhikkhu@sasanarakkha.org>
2022-10-03 07:35:59 +0800

    foxed help

--- bin/pdf2jpg.sh
+++ bin/pdf2jpg.sh
@@ -2,7 +2,7 @@
 f="$1"
 t="$2"
 [ -z "$res" ] && export res='1920x1080'
-[ -z "$1" -o "$1" = '-h' ] && echo "usage: [res='1024x640'] $0 from.pdf [to.jpg]" && exit 1
+[ -z "$1" -o "$1" = '-h' ] && echo "usage: [res='1920x1080'] $0 from.pdf [to.jpg]" && exit 1
 [ -z "$2" ] && t="${f%%.pdf}.jpg"
 
 convert -density 300 -size "$res" "$f" "$t"
