29ad698004c6afad83ba5a2a5a6f2ed37c9b4895
loki <jzouhar@netio/eu>
2023-03-09 22:43:58 +0530

    maildrlid p12 gen

--- bin/postsignum-p12-gen.sh
+++ bin/postsignum-p12-gen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 prefix="$1"
-openssl pkcs12 -export -out "${prefix}".p12 -inkey "${prefix}".key -in "${prefix}".pem -certfile postsignum_qca4_root.pem -certfile postsignum_qca4_sub.pem
 
-#or
-openssl pkcs12 -export -out "${prefix}".p12 -inkey "${prefix}".key -in "${prefix}".pem
+openssl pkcs12 -export -out "$prefix".p12 -inkey "$prefix".key -in "$prefix".pem
+
+
+openssl pkcs12 -export -out "$prefix"-maildroid.p12 -inkey "$prefix".key -in "$prefix".pem -legacy -twopass
+
