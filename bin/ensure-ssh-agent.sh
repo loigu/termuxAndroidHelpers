47842bc095ac6a72ceb22da4bb0325b5be5cea6a
loki <jzouhar@netio/eu>
2023-04-17 14:38:30 +0530

    improved agent presence detection

--- bin/ensure-ssh-agent.sh
+++ bin/ensure-ssh-agent.sh
@@ -2,11 +2,15 @@
 
 [ -z "$SSH_AUTH_SOCK" ] && export SSH_AUTH_SOCK="$HOME/.ssh_agent.sock"
 
+[ -z "$SSH_AGENT_PIDF" ] && export SSH_AGENT_PIDF="$HOME/.ssh_agent.pid"
+
+kill -0 $(cat "$SSH_AGENT_PIDF") &>/dev/null || rm -f "$SSH_AUTH_SOCK"
+
 if ! [ -S "$SSH_AUTH_SOCK" ];then
-	screen -dmS ssh-agent ssh-agent -D -a "$SSH_AUTH_SOCK"
+	screen -dmS ssh-agent bash -c 'echo $$ > "$SSH_AGENT_PIDF"; ssh-agent -D -a "$SSH_AUTH_SOCK"'
 	[ ! -S "$SSH_AUTH_SOCK" ] && sleep 1
 	ssh-add
 fi
 
-echo $SSH_AUTH_SOCK  >&2
+# echo $SSH_AUTH_SOCK  >&2
 
