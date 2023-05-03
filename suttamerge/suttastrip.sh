bd25969db79bf55ddf75d8a1bbbcdb3f1bbfb317
loki <jzouhar@netio/eu>
2023-05-03 15:48:01 +0530

    suttamerge simple html moszly done

--- suttamerge/suttastrip.sh
+++ suttamerge/suttastrip.sh
@@ -5,13 +5,13 @@ splittag="body"
 # remove head/tail
 # <body> .. </body>
 pl=$(grep "<$splittag" "$1"  -n  | cut -d : -f 1)
-pl=$(( $pl + 1))                     .../tinna_menio/pdf $ 
+pl=$(( $pl + 1))
 
 ll=$(grep "</$splittag" "$1"  -n  | cut -d : -f 1)
 ll=$(( $ll - 1))
 
 bn=$(basename "$1")
-echo "<div id=\"${bn&&.*}\">" > "$2"
+echo "<div id=\"${bn%%.*}\">" > "$2"
 head -n $ll "$1" | tail -n +$pl >> "$2"
 echo '</div>' >> "$2"
 
