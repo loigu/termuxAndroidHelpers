940500c3fe261b6102edc0f0853aea93883a6ced
loki <jzouhar@netio/eu>
2023-05-11 16:17:53 +0530

    website mirror local conversion script

---
+++ bin/website_mirror-link_local.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cmd="$1"
+base_dir=$(readlink -f "$2")
+base_domain=$(basename "$base_dir")
+
+
+
+function get_the_dots()
+{
+	slash=''
+	dir=$(dirname "$1")
+	while [ "$dir" != "$base_dir" ]; do
+		dir=$(dirname "$dir")
+		echo -n "$slash.."
+		slash='\/'
+	done
+}
+
+function prettyfi()
+{
+	tmp=$(mktemp)
+	find "$base_dir" -iname '*.html' |while read h;do
+		html_prettyprint.py "$h" "$tmp" && mv "$tmp" "$h" -f &>/dev/null
+	done
+	rm "$tmp"
+}
+
+function process()
+{
+	#todo: ./ in base directory
+	#main index g index.html sometimes missing
+	# src='' src=""
+	grep -rie "\"http.*$base_domain/.*\"" "$base_dir" | cut -d : -f 1 | sort -u | while read h; do
+		dots=$(get_the_dots "$h")
+		sed -i "$h" -e 's/\("https:\/\/'"$base_domain"'[^"]*\)\"/\1index.html"/g' -e 's/\("https:\\\/\\\/'"$base_domain"'[^"]*\)\"/\1index.html"/g'
+
+		sed -i "$h" -e 's/https:\/\/'"$base_domain/$dots/g" -e 's/https:\\\/\\\/'"$base_domain/$dots/g"
+	done
+}
+
+$cmd
+
