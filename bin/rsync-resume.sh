5b00023062fd7ab8d6023439e4b2acbf8e751d2f
loki <jzouhar@netio/eu>
2025-12-17 18:15:11 +0530

    use sparse if possible

--- bin/rsync-resume.sh
+++ bin/rsync-resume.sh
@@ -5,4 +5,4 @@ mode="--append-verify"
 
 [ -z "$2" -o "$1" = "-h" ] && echo -e $(basename "$0")" [-n] <in> <out>\n\t-n\t don't verify checksum when appending" && exit
 
-rsync -avz --compress-level=9 -P ${mode} "$@"
+rsync -avzsb --compress-level=9 -P ${mode} "$@"
